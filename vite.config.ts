45e59ba278f39963e5efd413d8495cb3540d2a5a
Styl Bandeira <stylbandeira@gmail.com>
2025-08-05 09:31:48 -0300

    Set it to port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
+    watch: {
+      usePolling: true,
+    }
   },
   plugins: [
     react(),
