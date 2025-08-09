ebb2e8b652d802579f617fab2fab8ccd944cbe96
Styl Bandeira <stylbandeira@gmail.com>
2025-08-09 17:44:25 -0300

    Add default headers and send token in every requisition

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,6 +3,21 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "http://localhost:8001/api",
     withCredentials: true,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
+
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    config.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
+    return config;
 });
 
 export default api;
