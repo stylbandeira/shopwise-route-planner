ae0698c01b26f21740b1bf64194b29661daa6e66
Styl Bandeira <stylbandeira@gmail.com>
2026-01-07 11:00:23 -0300

    Create arraysEqual util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function arraysEqual(a: number[], b: number[]) {
+  if (a.length !== b.length) return false;
+  return a.every((val, index) => val === b[index]);
+}
