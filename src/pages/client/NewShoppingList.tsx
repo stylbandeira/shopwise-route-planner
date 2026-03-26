eeab1321da1bc0661ca5338a1be6905eff9d7033
Styl Bandeira <stylbandeira@gmail.com>
2026-03-26 19:26:35 -0300

    Adjusts user favorites design

--- src/pages/client/NewShoppingList.tsx
+++ src/pages/client/NewShoppingList.tsx
@@ -314,8 +314,10 @@ export default function NewShoppingList({ isEditMode = false, listId }: NewShopp
                         >
                           <div className="flex-1">
                             <h3 className="font-semibold">{product.name}</h3>
+                            <span className="text-slate-400">{product.unity_quantity + " " + product.unity}</span>
                             <div className="flex items-center gap-2 mt-1">
-                              <Badge variant="secondary">{product.category}</Badge>
+                              <Badge variant="outline">{product.category}</Badge>
+                              <Badge variant={product.mentioned_quantity_variant ?? "destructive"}>Precisão: {product.mentioned_quantity}</Badge>
                             </div>
                             <div className="flex items-center gap-4 mt-2 text-sm">
                               <span className="font-bold text-primary">R$ {product.average_price.toFixed(2)}</span>
